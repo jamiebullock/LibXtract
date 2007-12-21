672302aadaba97f3e5df256e3d358cb2411ab3ed
Jamie Bullock <jamie@postlude.co.uk>
2007-12-21 11:05:20 +0000

    - Added some previously uncommitted Pd testing examples - Improved python bindings. Now we can do vector features as well as scalar from Python

--- swig/python/test.py
+++ swig/python/test.py
@@ -3,11 +3,11 @@
 try:
     import xtract
 except ImportError:
-    print 'Failed to load the library "jxtract"'
+    print 'Failed to load the library "xtract"'
 
 print '\nRunning libxtract Python bindings test...\n'
 
-len = 5
+len = 8
 
 a = xtract.floatArray(len)
 temp = []
@@ -18,5 +18,20 @@ for i in range(0, len):
 
 print 'The mean of ' + ', '.join(temp) + ' is: %.2f' % \
 	  xtract.xtract_mean(a,len,None)[1]
+
+print 'Computing spectrum...'
+
+argv = xtract.floatArray(1)
+argv[0] = 44100.0 / len  # Fake sample rate
+
+xtract.xtract_init_fft(len, xtract.XTRACT_SPECTRUM);
+
+result = xtract.floatArray(len)
+
+xtract.xtract_spectrum(a,len,argv, result)
+
+
+for i in range(len):
+    print result[i]
 
 print '\nFinished!\n'
--- swig/xtract.i
+++ swig/xtract.i
@@ -4,10 +4,19 @@
 
 %{
 #include "xtract/xtract_scalar.h"
+#include "xtract/xtract_vector.h"
+#include "xtract/libxtract.h"
 %}
 
 %array_class(float, floatArray); 
 %apply float *OUTPUT { float *result };
 /* %apply float *INPUT { float *data }; */
 
+%ignore xtract;
+
 %include "xtract/xtract_scalar.h"
+
+%clear float *result;
+
+%include "xtract/xtract_vector.h"
+%include "xtract/libxtract.h"
