a0675469cc2092cf39b39ea816e37cfc08a0bddf
Jamie Bullock <jamie@jamiebullock.com>
2013-06-18 08:16:45 -0700

    Update function prototype to use const for passed in sample buffer

--- src/dywapitchtrack/dywapitchtrack.h
+++ src/dywapitchtrack/dywapitchtrack.h
@@ -100,7 +100,7 @@ void dywapitch_inittracking(dywapitchtracker *pitchtracker);
 // startsample : the index of teh first sample to use in teh sample buffer
 // samplecount : the number of samples to use to compte the pitch
 // return 0.0 if no pitch was found (sound too low, noise, etc..)
-double dywapitch_computepitch(dywapitchtracker *pitchtracker, double * samples, int startsample, int samplecount);
+double dywapitch_computepitch(dywapitchtracker *pitchtracker, const double * samples, int startsample, int samplecount);
 
 #ifdef __cplusplus
 } // extern "C"
