c866e1676d423ca1c6d4aa859dd54df487ab25d4
Jamie Bullock <jamie@jamiebullock.com>
2014-06-03 21:17:07 +0100

    Add "stateful" feature type with initial feature "last n"
    Stateful feature extraction functions are functions that require state to be maintained between successive calls.  This is necessary, for example when an accumulation of values is required, or changes need to be measured over time.
    
    The initial xtract_last_n() function accumulates the last N (single) values from *data and writes them to *result

---
+++ src/stateful.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2012 Jamie Bullock
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+/* stateful.c: declares functions that extract features that require stateful data to be retained between frames */
+
+#include "../xtract/xtract_stateful.h"
+#include "../xtract/libxtract.h"
+
+#include "c-ringbuf/ringbuf.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <float.h>
+
+struct xtract_last_n_state_
+{
+    ringbuf_t ringbuf;
+};
+
+
+xtract_last_n_state *xtract_last_n_state_new(size_t N)
+{
+    xtract_last_n_state *last_n_state = malloc(sizeof(xtract_last_n_state));
+    
+    if (last_n_state == NULL)
+    {
+        perror("could not allocate memory for xtract_last_n_state");
+        return NULL;
+    }
+    
+    last_n_state->ringbuf = ringbuf_new(N * sizeof(double));
+    
+    if (last_n_state->ringbuf)
+    {
+        perror("could not allocate memory for xtract_last_n_state->ringbuf");
+    }
+        
+    return last_n_state;
+}
+
+void xtract_last_n_state_delete(xtract_last_n_state *last_n_state)
+{
+    ringbuf_free(&last_n_state->ringbuf);
+    free(last_n_state);
+}
+
+int xtract_last_n(const xtract_last_n_state *state, const double *data, const int N, const void *argv, double *result)
+{
+    double *current = (double *)argv;
+    size_t N_bytes = N * sizeof(double);
+    
+    if (N_bytes != ringbuf_capacity(state->ringbuf))
+    {
+        fprintf(stderr, "libxtract: error: xtract_last_n(): inconsitent size");
+        return XTRACT_BAD_STATE;
+    }
+    
+    ringbuf_memcpy_into(state->ringbuf, current, sizeof(double));
+    size_t used = ringbuf_bytes_used(state->ringbuf);
+    ringbuf_memcpy_from(result, state->ringbuf, used, false);
+    
+    if (used < N_bytes)
+    {
+        /* zero pad */
+        for (size_t n = used / sizeof(double); n < N; ++n)
+        {
+            result[n] = 0.0;
+        }
+    }
+    
+    return XTRACT_SUCCESS;
+}
+
---
+++ xtract/xtract_stateful.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2012 Jamie Bullock
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+/** \file xtract_stateful.h: declares functions that extract features that require stateful data to be retained between frames */
+#ifndef XTRACT_STATEFUL_H
+#define XTRACT_STATEFUL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+/**
+ * \defgroup stateful feature extraction functions and data structures
+ *
+ * Functions that extract a feature over multiple frames
+ *
+ * @{
+ */
+
+    
+#include <stdint.h>
+#include <string.h>
+
+struct ringbuf_t;
+typedef struct ringbuf_t *ringbuf_t;
+typedef struct xtract_last_n_state_ xtract_last_n_state;
+        
+xtract_last_n_state *xtract_last_n_state_new(size_t capacity);
+void xtract_last_n_state_delete(xtract_last_n_state *last_n_state);
+
+    
+/**
+ *  Write a vector of the last N input values to `result`
+ *
+ *  @param state  a pointer to an xtract_peak_picker_state struct as allocated by xtract_peak_picker_state_new()
+ *  @param data   a pointer to a double representing the current input value
+ *  @param N      an integer representing 'N' the number of values to be written to *result
+ *  @param argv   a pointer to NULL
+ *  @param result a pointer to an array of doubles representing the last N values, where the nth value is the current one. The array must have been allocated to size N elements and initialised by the caller
+ *
+ */
+int xtract_last_n(const xtract_last_n_state *state, const double *data, const int N, const void *argv, double *result);
+    
+
+
+
+#endif
