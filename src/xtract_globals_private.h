47b7b283b2aac9577ec085a3bafe85eebe6c7826
Csaba Kertesz <csaba.kertesz@gmail.com>
2015-09-15 23:49:32 +0300

    Support multiple threads

--- src/xtract_globals_private.h
+++ src/xtract_globals_private.h
@@ -32,22 +32,42 @@
 #ifdef __cplusplus
 #define GLOBAL extern "C"
 #else
-#define GLOBAL
+#define GLOBAL extern
+#endif
+
+// From http://stackoverflow.com/questions/18298280/how-to-declare-a-variable-as-thread-local-portably/18298965#18298965
+#ifndef thread_local
+# if __STDC_VERSION__ >= 201112 && !defined __STDC_NO_THREADS__
+#  define thread_local _Thread_local
+# elif defined _WIN32 && ( \
+       defined _MSC_VER || \
+       defined __ICL || \
+       defined __DMC__ || \
+       defined __BORLANDC__ )
+#  define thread_local __declspec(thread) 
+/* note that ICC (linux) and Clang are covered by __GNUC__ */
+# elif defined __GNUC__ || \
+       defined __SUNPRO_C || \
+       defined __xlC__
+#  define thread_local __thread
+# else
+#  error "Cannot define thread_local"
+# endif
 #endif
 
 #ifdef USE_OOURA
-GLOBAL struct xtract_ooura_data_ ooura_data_dct;
-GLOBAL struct xtract_ooura_data_ ooura_data_mfcc;
-GLOBAL struct xtract_ooura_data_ ooura_data_spectrum;
-GLOBAL struct xtract_ooura_data_ ooura_data_autocorrelation_fft;
+GLOBAL thread_local struct xtract_ooura_data_ ooura_data_dct;
+GLOBAL thread_local struct xtract_ooura_data_ ooura_data_mfcc;
+GLOBAL thread_local struct xtract_ooura_data_ ooura_data_spectrum;
+GLOBAL thread_local struct xtract_ooura_data_ ooura_data_autocorrelation_fft;
 #else
-GLOBAL xtract_vdsp_data vdsp_data_dct;
-GLOBAL xtract_vdsp_data vdsp_data_mfcc;
-GLOBAL xtract_vdsp_data vdsp_data_spectrum;
-GLOBAL xtract_vdsp_data vdsp_data_autocorrelation_fft;
+GLOBAL thread_local xtract_vdsp_data vdsp_data_dct;
+GLOBAL thread_local xtract_vdsp_data vdsp_data_mfcc;
+GLOBAL thread_local xtract_vdsp_data vdsp_data_spectrum;
+GLOBAL thread_local xtract_vdsp_data vdsp_data_autocorrelation_fft;
 #endif
 
-GLOBAL dywapitchtracker wavelet_f0_state;
+GLOBAL thread_local dywapitchtracker wavelet_f0_state;
 
 
 #endif /* Header guard */
