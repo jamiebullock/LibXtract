1bd06edea5d6874c21f2307712d5e3861e09ebe9
Csaba Kertesz <csaba.kertesz@gmail.com>
2016-01-28 15:03:26 +0200

    Implemented centroid calculation of spectral mel bands

--- include/xtract/xtract_vector.h
+++ include/xtract/xtract_vector.h
@@ -135,6 +135,17 @@ int xtract_peak_spectrum(const double *data, const int N, const void *argv, doub
  */
 int xtract_harmonic_spectrum(const double *data, const int N, const void *argv, double *result);
 
+/** \brief Extract centroids of the spectral mel subbands
+ *
+ * \param *data: a pointer to the first element in an array of doubles representing the spectrum of an audio vector, (e.g. the array pointed to by *result from xtract_spectrum(), xtract_peak_spectrum() or xtract_harmonic_spectrum()).
+ * \param N: the number of array elements to be considered
+ * \param *argv: a pointer to a data structure of type xtract_mel_filter, containing n_filters coefficient tables to make up a mel-spaced filterbank
+ * \param *result: a pointer to an array containing the spectral band centroids
+ *
+ * Note: for a more 'accurate' result *result from xtract_peak_spectrum() can be passed in. This gives the interpolated peak frequency locations.
+ */
+int xtract_spectral_subband_centroids(const double *data, const int N, const void *argv, double *result);
+
 /** \brief Extract Linear Predictive Coding Coefficients
  * 
  * Based on algorithm in Rabiner and Juang as implemented by Jutta Degener in Dr. Dobb's Journal December, 1994.
