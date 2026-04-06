8353472f3580bdec69f5537ae7152b452f4eb9f1
Jamie Bullock <jamie.b.bullock@gmail.com>
2026-04-06 20:17:20 +0100

    fix: scale wavelet_f0 result for actual samplerate
    dywapitchtrack assumes 44100 Hz internally. The samplerate from
    argv was read but never used. Scale the result as recommended
    by the dywapitchtrack documentation.
    
    Fixes #56

--- src/scalar.c
+++ src/scalar.c
@@ -1116,7 +1116,7 @@ int xtract_failsafe_f0(const double *data, const int N, const void *argv, double
 
 int xtract_wavelet_f0(const double *data, const int N, const void *argv, double *result)
 {
-    /* double sr = *(double *)argv; */
+    double sr = *(double *)argv;
 
     *result = dywapitch_computepitch(&wavelet_f0_state, data, 0, N);
 
@@ -1124,6 +1124,9 @@ int xtract_wavelet_f0(const double *data, const int N, const void *argv, double 
     {
         return XTRACT_NO_RESULT;
     }
+
+    /* dywapitchtrack assumes 44100 Hz internally; scale for actual samplerate */
+    *result *= sr / 44100.0;
 
     return XTRACT_SUCCESS;
 }
